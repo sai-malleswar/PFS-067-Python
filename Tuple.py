d154a741fdba5a5efc4b747d0566e052d2e3ed8f
Sai Malleswar <saimalleswar.m@gmail.com>
2026-09-12 12:21:25 +0530

    Tuple

---
+++ Tuple.py
@@ -0,0 +1,44 @@
+'''Tuple:
+student = (10,"Sai")
+print(student)'''
+
+'''set
+hell = {10, 20, 30, 40, 50, 50, 50, "sai", "sai"}
+print(hell)'''
+
+'''Dictionary
+student = {"name" : "sai",
+           "age" : "21",
+           "place" : "vij"
+           }
+print(student)'''
+
+'''a = int(input("Enter your age:"))
+print(a)
+print(type(a))'''
+
+#converting string to integer
+'''x="100"
+y= int(x)
+print(y)
+print(type(y))'''
+
+#int to float
+'''a=100
+b=float(a)
+print(b)
+print(type(b))'''
+
+#int to string
+'''a=100
+b=str(a)
+print(b)
+print(type(b))'''
+
+'''a=list(("saimalleswar"))
+print(a)
+print(type(a))'''
+
+
+
+
