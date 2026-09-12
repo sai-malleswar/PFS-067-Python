6591c034f3dbc1e12ceda0adc9d7cda681bdfba3
Sai Malleswar <saimalleswar.m@gmail.com>
2026-09-12 12:45:43 +0530

    operators

---
+++ operators.py
@@ -0,0 +1,30 @@
+#Arithemetic operators
+'''a=10
+b=4
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a//b)
+print(a%b)
+print(a**b)'''
+
+#Assignment Operators
+'''a=5
+b=3
+print(a==b)
+print(a!=b)
+print(a>b)
+print(a<b)
+print(a>=b)
+print(b<=a)'''
+
+'''price = 1000
+quantity = 3
+total = price * quantity 
+if total>=3000:
+    discount = total *0.10
+else:
+    discount = 0
+final_price = total - discount
+print(final_price)'''
