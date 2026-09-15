cbd8c3fb083a7099f164053d1eebde6d59fedb55
Sai Malleswar <saimalleswar.m@gmail.com>
2026-09-15 14:48:07 +0530

    Strings

---
+++ Strings.py
@@ -0,0 +1,47 @@
+'''name="Sai Rajeswar"
+print(name)
+print(name[5])
+print(name[-3])
+print(name[-8])
+print(name[0])
+print(name[2])'''
+
+'''Complex:
+a = 3 + 4j
+print(a.real)
+print(a.imag)'''
+
+'''Slicing:
+Word = "Sai Malleswar"
+print(Word[0:7])'''
+
+'''List:
+list1 = ["Telugu", "Hindi", "English"]
+list1[2] = "Maths"
+print(list1)
+
+Student1 = [28, "Sai","Vijayawada"]
+Student2 = [14,"Raju","Vijayawada"]
+print(Student1,Student2)'''
+
+'''append:
+student = [10,20,30,40]
+print(student)
+student.append(50)
+print(student)'''
+
+'''extend:
+a=[10,20,30]
+b=[40,50,60]
+a.extend(b)
+print(a)'''
+
+'''pop:
+a=[10,20,30,40,50]
+a.pop()
+print(a)'''
+
+'''remove:
+a=[10,20,30,40]
+a.remove(30)
+print(a)'''
